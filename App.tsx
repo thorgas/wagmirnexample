34691567cabdc082fff6ab799452533d492a81ad
thorgas <thorgas@users.noreply.github.com>
2021-10-22 11:39:25 +0100

    first commit

---
+++ App.tsx
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+import 'react-native-gesture-handler';
+import React from 'react';
+import {SafeAreaView, StatusBar, useColorScheme, View} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {LineChart} from 'react-native-wagmi-charts';
+
+const data = [
+  {
+    timestamp: 1625945400000,
+    value: 33575.25,
+  },
+  {
+    timestamp: 1625946300000,
+    value: 33545.25,
+  },
+  {
+    timestamp: 1625947200000,
+    value: 33510.25,
+  },
+  {
+    timestamp: 1625948100000,
+    value: 33215.25,
+  },
+];
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View
+        style={{
+          backgroundColor: isDarkMode ? Colors.black : Colors.white,
+        }}>
+        <LineChart.Provider data={data}>
+          <LineChart>
+            <LineChart.Path />
+            <LineChart.CursorLine />
+            <LineChart.PriceText />
+            <LineChart.DatetimeText />
+          </LineChart>
+        </LineChart.Provider>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
